8837c5a84e1edc0b7fbd97a7fbd0495a47291c27
karmraj patel <karmraj13579@gmail.com>
2026-04-17 22:34:27 +0530

    upload

---
+++ list.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class list {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(90);
+        list.add(43);
+        list.add(73);
+
+        System.out.print(list);
+    }
+}
